e98daff2cfad6ea159b5969da69f17a9c459d014
Vitalii Braun <vitossvx@gmail.com>
2023-09-26 23:51:24 +0300

    work in progress

--- src/CanvasMap/MapCanvas.class.ts
+++ src/CanvasMap/MapCanvas.class.ts
@@ -1,3 +1,5 @@
+type Point = { x: number; y: number };
+
 function diffPoints(p1: Point, p2: Point) {
   return { x: p1.x - p2.x, y: p1.y - p2.y };
 }
@@ -14,7 +16,19 @@ function scalePoint(p1: Point, scale: number) {
   return { x: p1.x / scale, y: p1.y / scale };
 }
 
-type Point = { x: number; y: number };
+function offsetImgWhileZoom(
+  imgCoords: Point,
+  mousePos: Point,
+  // scale: number,
+  canvasSize: { height: number; width: number }
+) {
+  const mousePosPerc = {
+    x: (mousePos.x * 100) / canvasSize.width,
+    y: (mousePos.y * 100) / canvasSize.height,
+  };
+  console.log(mousePosPerc);
+  return mousePosPerc;
+}
 
 const ZOOM_SENSITIVITY = 400;
 
@@ -22,27 +36,24 @@ const MAX_DELTAY = 50;
 
 const ORIGIN = { x: 0, y: 0 };
 
-const IMAGE_WIDTH = 3000;
-
 export class MapCanvas {
   ctx: CanvasRenderingContext2D | null;
   scale: number = 1;
-  relativeScale: number = 1;
   img = new Image();
   imgCoords: Point = { x: 0, y: -200 };
   mousePos: Point = ORIGIN;
-  viewportTopLeft: Point = ORIGIN;
   lastMousePos: Point = ORIGIN;
-  offset: Point = ORIGIN;
-  absoluteOffset: Point = ORIGIN;
+  currentMousePos: Point = ORIGIN;
   isPanning: boolean = false;
-
-  // renderLoop: NodeJS.Timer;
+  canvasSize: { height: number; width: number };
+  imgSize: { height: number; width: number };
 
   constructor(private canvas: HTMLCanvasElement) {
     this.ctx = canvas.getContext("2d");
     this.img.src = "map.png";
     this.addEventListeners();
+    this.canvasSize = { height: this.canvas.height, width: this.canvas.width };
+    this.imgSize = { height: this.canvas.width, width: this.canvas.width };
     this.img.onload = () => {
       this.render();
     };
@@ -58,8 +69,8 @@ export class MapCanvas {
         this.img,
         this.imgCoords.x,
         this.imgCoords.y,
-        this.canvas.width * this.scale,
-        this.canvas.width * this.scale
+        this.imgSize.width * this.scale,
+        this.imgSize.width * this.scale
       );
     }
     requestAnimationFrame(this.render.bind(this));
@@ -77,80 +88,70 @@ export class MapCanvas {
     }
   }
 
-  handleUpdateMouseByTouch(e: TouchEvent) {
-    e.preventDefault();
-    if (this.canvas) {
-      const touch = e.touches[0];
-      if (touch) {
-        const viewportMousePos = {
-          x: touch.clientX,
-          y: touch.clientY,
-        };
-        const topLeftCanvasPos = {
-          x: this.canvas.offsetLeft,
-          y: this.canvas.offsetTop,
-        };
-        this.mousePos = diffPoints(viewportMousePos, topLeftCanvasPos);
-      }
-    }
-  }
-
   mouseMove(e: MouseEvent) {
-    if (this.ctx && this.isPanning) {
-      const lastMousePos = this.lastMousePos;
-      const currentMousePos = { x: e.pageX, y: e.pageY };
-
-      this.lastMousePos = currentMousePos;
-
-      const mouseDiff = diffPoints(currentMousePos, lastMousePos);
-
-      const newImgCoords = addPoints(this.imgCoords, mouseDiff);
-
-      const imgRightCornerCoords =
-        newImgCoords.x + this.canvas.width * this.scale;
-
-      const isMapOutsideVeiwFrame =
-        newImgCoords.x > 0 || imgRightCornerCoords < this.canvas.width;
-
-      if (isMapOutsideVeiwFrame) {
-        return;
+    if (this.ctx) {
+      this.currentMousePos = { x: e.pageX, y: e.pageY };
+      if (this.isPanning) {
+        this.mapMove();
       }
-
-      this.imgCoords = newImgCoords;
-
-      // console.log(this.imgCoords);
+      this.lastMousePos = this.currentMousePos;
     }
   }
 
-  // touchMove(e: TouchEvent) {
-  //   // console.log("start move", this.isPanning);
-  //   if (this.ctx && this.isPanning) {
-  //     const lastMousePos = this.lastMousePos;
-  //     const touch = e.touches[0];
+  mapMove() {
+    const mouseDiff = diffPoints(this.currentMousePos, this.lastMousePos);
+    const newImgCoords = addPoints(this.imgCoords, mouseDiff);
+    if (!this.boundMapMove(newImgCoords)) {
+      return;
+    }
+    this.imgCoords = newImgCoords;
+  }
 
-  //     if (touch) {
-  //       const currentMousePos = { x: touch.clientX, y: touch.clientY };
-  //       this.lastMousePos = currentMousePos;
-  //       const mouseDiff = diffPoints(currentMousePos, lastMousePos);
-  //       this.ctx.translate(mouseDiff.x, mouseDiff.y);
-  //       this.render();
-  //     }
-  //   }
-  // }
+  boundMapMove(newImgCoords: Point = this.imgCoords) {
+    const absoluteImgRectCoords = {
+      x: newImgCoords.x,
+      y: newImgCoords.y,
+      width: this.imgSize.width * this.scale,
+      height: this.imgSize.height * this.scale,
+    };
 
-  mouseUp(e: MouseEvent) {
+    const absoluteCanvasRectCoords = {
+      ...ORIGIN,
+      width: this.canvasSize.width,
+      height: this.canvasSize.height,
+    };
+    if (
+      absoluteImgRectCoords.width + absoluteImgRectCoords.x <
+      absoluteCanvasRectCoords.width
+    ) {
+      return false;
+    }
+    if (
+      absoluteImgRectCoords.height + absoluteImgRectCoords.y <
+      absoluteCanvasRectCoords.height
+    ) {
+      return false;
+    }
+    if (absoluteImgRectCoords.x > absoluteCanvasRectCoords.x) {
+      return false;
+    }
+    if (absoluteImgRectCoords.y > absoluteCanvasRectCoords.y) {
+      return false;
+    }
+
+    return true;
+  }
+
+  mouseUp() {
     this.isPanning = false;
     this.canvas.removeEventListener("mousemove", this.mouseMove);
     document.removeEventListener("mouseup", this.mouseUp);
   }
 
   startPan(e: MouseEvent) {
-    // console.log("start pan");
-    this.canvas.addEventListener("mousemove", this.mouseMove.bind(this));
-    // this.canvas.addEventListener("touchmove", this.touchMove.bind(this));
+    this.isPanning = true;
     document.addEventListener("mouseup", this.mouseUp.bind(this));
     this.lastMousePos = { x: e.pageX, y: e.pageY };
-    this.isPanning = true;
   }
 
   handleWheel(e: WheelEvent) {
@@ -163,6 +164,9 @@ export class MapCanvas {
 
       //e.deltaY - разница значения при прокрутке колесом
       const zoom = 1 - deltaY / ZOOM_SENSITIVITY;
+      if (!this.boundMapMove()) {
+        return;
+      }
 
       if (this.scale * zoom <= 1) {
         this.scale = 1;
@@ -174,19 +178,18 @@ export class MapCanvas {
       }
 
       this.scale = this.scale * zoom;
-
-      // this.render();
     }
   }
 
   addEventListeners() {
+    this.canvas.addEventListener("mousemove", this.mouseMove.bind(this));
     this.canvas.addEventListener("wheel", this.handleWheel.bind(this));
     this.canvas.addEventListener("mousedown", this.startPan.bind(this));
   }
 
   removeEventListeners() {
     this.canvas.removeEventListener("wheel", this.handleWheel);
+    this.canvas.removeEventListener("mousemove", this.mouseMove.bind(this));
     this.canvas.removeEventListener("mousedown", this.startPan.bind(this));
-    // clearInterval(this.renderLoop);
   }
 }
