722d9de8ab36fe58829ae2b6b4e3c417be831ba2
Vitalii Braun <vitossvx@gmail.com>
2023-10-02 15:59:53 +0300

    Add texts to point

--- src/Kanva.tsx
+++ src/Kanva.tsx
@@ -1,7 +1,7 @@
 import { KonvaEventObject } from "konva/lib/Node";
-import React, { useRef, useState } from "react";
+import React, { useRef } from "react";
 import Konva from "konva";
-import { Stage, Layer, Image, Circle } from "react-konva";
+import { Stage, Layer, Image, Circle, Text } from "react-konva";
 import useImage from "use-image";
 import { Vector2d } from "konva/lib/types";
 
@@ -18,9 +18,36 @@ const IMAGE_SIZE = {
 const MIN_SCALE = STAGE_SIZE.width / IMAGE_SIZE.width;
 const MAX_SCALE = 5;
 
-const points = [
-  { id: 1, pos: { x: 1500, y: 1600 } },
-  { id: 2, pos: { x: 1600, y: 1700 } },
+type TextPosition =
+  | "left"
+  | "right"
+  | "top"
+  | "left-top"
+  | "right-top"
+  | "bottom"
+  | "bottom-left"
+  | "bottom-right";
+
+type Point = {
+  id: number;
+  pos: Vector2d;
+  text: string;
+  position: TextPosition;
+};
+
+const points: Point[] = [
+  {
+    id: 1,
+    pos: { x: 1500, y: 1600 },
+    text: "Улица Правды 24",
+    position: "left-top",
+  },
+  {
+    id: 2,
+    pos: { x: 1600, y: 1700 },
+    text: "Улица Правды 20",
+    position: "left-top",
+  },
 ];
 
 function offsetStagePositionInBounds(
@@ -63,39 +90,41 @@ function offsetStagePositionInBounds(
 export default function Kanva() {
   const stageRef = useRef<Konva.Stage>(null);
   const [mapImage] = useImage("./map.png");
+
   const handleWheel = (e: KonvaEventObject<WheelEvent>) => {
     const scaleBy = 1.05;
-    if (stageRef.current) {
-      e.evt.preventDefault();
-
-      const oldScale = stageRef.current.scaleX();
-      const pointer = stageRef.current.getPointerPosition();
-
-      const mousePointTo = {
-        x: (pointer!.x - stageRef.current.x()) / oldScale,
-        y: (pointer!.y - stageRef.current.y()) / oldScale,
-      };
-
-      let direction = -(e.evt.deltaY > 0 ? 1 : -1);
-
-      const newScale = direction > 0 ? oldScale * scaleBy : oldScale / scaleBy;
-
-      if (newScale < MIN_SCALE || newScale > MAX_SCALE) {
-        return;
-      }
-
-      stageRef.current.scale({ x: newScale, y: newScale });
-
-      const newPos = {
-        x: pointer!.x - mousePointTo.x * newScale,
-        y: pointer!.y - mousePointTo.y * newScale,
-      };
-
-      stageRef.current.position(offsetStagePositionInBounds(newPos, newScale));
+    if (!stageRef.current) {
+      return;
     }
+    e.evt.preventDefault();
+
+    const oldScale = stageRef.current.scaleX();
+    const pointer = stageRef.current.getPointerPosition();
+
+    const mousePointTo = {
+      x: (pointer!.x - stageRef.current.x()) / oldScale,
+      y: (pointer!.y - stageRef.current.y()) / oldScale,
+    };
+
+    let direction = -(e.evt.deltaY > 0 ? 1 : -1);
+
+    const newScale = direction > 0 ? oldScale * scaleBy : oldScale / scaleBy;
+
+    if (newScale < MIN_SCALE || newScale > MAX_SCALE) {
+      return;
+    }
+
+    stageRef.current.scale({ x: newScale, y: newScale });
+
+    const newPos = {
+      x: pointer!.x - mousePointTo.x * newScale,
+      y: pointer!.y - mousePointTo.y * newScale,
+    };
+
+    stageRef.current.position(offsetStagePositionInBounds(newPos, newScale));
   };
 
-  const handleMoveMap = (pos: Vector2d): Vector2d => {
+  const handleMoveStage = (pos: Vector2d): Vector2d => {
     if (stageRef.current) {
       const currentScale = stageRef.current.scaleX();
       return offsetStagePositionInBounds(pos, currentScale);
@@ -110,7 +139,7 @@ export default function Kanva() {
         {...STAGE_SIZE}
         onWheel={handleWheel}
         draggable
-        dragBoundFunc={handleMoveMap}
+        dragBoundFunc={handleMoveStage}
         scale={{ x: 0.5, y: 0.5 }}
       >
         <Layer>
@@ -118,7 +147,16 @@ export default function Kanva() {
         </Layer>
         <Layer>
           {points.map((point) => (
-            <Circle x={point.pos.x} y={point.pos.y} fill="red" radius={10} />
+            <>
+              <Text
+                text={point.text}
+                fill="white"
+                x={point.pos.x}
+                y={point.pos.y}
+                fontSize={40}
+              />
+              <Circle x={point.pos.x} y={point.pos.y} fill="red" radius={10} />
+            </>
           ))}
         </Layer>
       </Stage>
