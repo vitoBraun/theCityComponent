7577aad968e43a7c120b9506ec7609da86b8f8de
Vitalii Braun <vitossvx@gmail.com>
2023-10-03 12:30:35 +0300

    Cleanup

--- src/KonvaMap/TheCityMap.tsx
+++ src/KonvaMap/TheCityMap.tsx
@@ -1,7 +1,7 @@
 import { KonvaEventObject } from "konva/lib/Node";
 import React, { useRef, useState } from "react";
 import Konva from "konva";
-import { Stage, Layer, Image, Circle, Text, Group, Rect } from "react-konva";
+import { Stage, Layer, Image, Circle, Text, Group } from "react-konva";
 import useImage from "use-image";
 import { Vector2d } from "konva/lib/types";
 
@@ -90,7 +90,6 @@ export default React.memo(({ maxScale, stageSize, minScale }: MapProps) => {
   const handleMoveStage = (pos: Vector2d): Vector2d => {
     if (stageRef.current) {
       const currentScale = stageRef.current.scaleX();
-      console.log(pos);
       return getBoundedStagePosition(
         pos,
         currentScale,
